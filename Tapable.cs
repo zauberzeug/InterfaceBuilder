b564b3a8e61d23977bdd3404e9e6fca539c769ab
Rodja Trappe <rodja@perpetual-mobile.de>
2018-06-16 07:12:52 +0200

    introducing async OnTap Action

--- Tapable.cs
+++ Tapable.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using System.Linq;
 
 namespace InterfaceBuilder
 {
@@ -8,6 +11,23 @@ namespace InterfaceBuilder
         public static T OnTap<T>(this T view, Action action) where T : View
         {
             view.GestureRecognizers.Add(new TapGestureRecognizer() { Command = new Command(action) });
+            return view;
+        }
+
+        public static T OnTap<T>(this T view, Func<Task> func) where T : View
+        {
+            view.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(async () =>
+                {
+                    view.Opacity = 0.5;
+                    var gestureRecognizers = view.GestureRecognizers.ToList();
+                    view.GestureRecognizers.Clear(); // NOTE: disable other gestures while we execute this action
+                    await func.Invoke();
+                    gestureRecognizers.ForEach(g => view.GestureRecognizers.Add(g));
+                    view.Opacity = 1;
+                })
+            });
             return view;
         }
     }
