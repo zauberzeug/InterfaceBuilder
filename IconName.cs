081acf0304ecb4f887065067e8b07105deeb9baa
Rodja Trappe <rodja@perpetual-mobile.de>
2018-06-17 04:40:19 +0200

    renaming

---
+++ IconName.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace InterfaceBuilder
+{
+    // http://elusiveicons.com/icons/
+    public static class IconName
+    {
+        public static readonly string Checkmark = "";
+        public static readonly string Plus = "";
+        public static readonly string Trash = "";
+        public static readonly string ChevronUp = "";
+        public static readonly string ChevronDown = "";
+        public static readonly string ChevronRight = "";
+        public static readonly string Magnifier = "";
+        public static readonly string Telephone = "";
+        public static readonly string Email = "";
+        public static readonly string Play = "";
+        public static readonly string Pause = "";
+        public static readonly string Stop = "";
+        public static readonly string RemoveCircle = "";
+        public static readonly string Camera = "";
+        public static readonly string Circle = "";
+        public static readonly string ArrowUpDown = "";
+        public static readonly string Key = "";
+        public static readonly string InfoCircle = "";
+        public static readonly string Dashboard = "";
+        public static readonly string FileEdit = "";
+        public static readonly string Signal = "";
+        public static readonly string Gear = "";
+        public static readonly string Remove = "";
+    }
+}
