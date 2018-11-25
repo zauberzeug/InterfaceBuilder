2eecb6e38f4fc4cf5da69885ad4807cfc8bceb0a
Rodja Trappe <rodja@perpetual-mobile.de>
2018-11-25 06:26:20 +0100

    added solution with demo projects

---
+++ Demo/App.cs
@@ -0,0 +1,14 @@
+﻿using InterfaceBuilder;
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var ui = new UI();
+            MainPage = ui.Page("mainpage", ui.Label("test"));
+        }
+    }
+}
