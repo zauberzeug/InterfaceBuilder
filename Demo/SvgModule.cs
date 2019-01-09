b909f0c8562bc5c3ad8cdaa8ba7f52ddad4ad7ff
Rodja Trappe <rodja@perpetual-mobile.de>
2019-01-09 13:01:16 +0100

    forgot new files

---
+++ Demo/SvgModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using InterfaceBuilder;
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public class SvgModule : Module
+    {
+        UI ui;
+
+        public SvgModule(UI ui) : base(ui)
+        {
+            this.ui = ui;
+            Icon = IconName.Brush;
+
+            Page = ui.NavigationPage(UI.Page("SVG Graphics", ui.Stack().With(
+
+                ui.Headline("From EmbeddedResource"),
+                ui.Svg("logo.svg").Height(200),
+
+                ui.Headline("Remarks"),
+                ui.Label("To load svg grapics you need to initialize the renderer wit SvgImageRenderer.Init(); " +
+                         "after calling Xamarin.Forms.Init(); on each plattform."),
+                ui.Label("Also note that the svg library is a bit picky with the size definition inside the svg. " +
+                         "make sure the size parameters are definded in px not mm or someting else.")
+            )));
+        }
+    }
+}
